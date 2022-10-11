2c9e185e63c870397e9f66bf4d808df396c29471
ahsan ali <syedahsansaeed@gmail.com>
2022-10-11 20:17:19 +0500

    fixed alingo thank you title

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -1,27 +1,33 @@
-import React from "react"
+import React from "react";
 import { useEffect } from "react";
 
-function Thanks(){
-
+function Thanks() {
   // Tab Title
-	useEffect(() => {
-		document.title = "ALigno-Thanks";  
-	  }, []);
+  useEffect(() => {
+    document.title = "Aligno-Thanks";
+  }, []);
 
-    return(
-       <>
-       <div class="jumbotron text-center">
-  <h1 class="display-3">Thank You!</h1>
-  {/* <p class="lead"><strong>Please check your email</strong> for further instructions on how to complete your account setup.</p> */}
-  <hr/>
-  {/* <p>
+  return (
+    <>
+      <div class="jumbotron text-center">
+        <h1 class="display-3">Thank You!</h1>
+        {/* <p class="lead"><strong>Please check your email</strong> for further instructions on how to complete your account setup.</p> */}
+        <hr />
+        {/* <p>
     Having trouble? <a href="https://bootstrapcreative.com/">Contact us</a>
   </p> */}
-  <p class="lead">
-    <a className="btn  btn-sm" style={{ backgroundColor:`#01aba9`}} href="https://portal.aligno.co/rx-form/" role="button">Back to RX-Form</a>
-  </p>
-</div>
-       </>       
-    )
-};
-export default Thanks
+        <p class="lead">
+          <a
+            className="btn  btn-sm"
+            style={{ backgroundColor: `#01aba9` }}
+            href="https://portal.aligno.co/rx-form/"
+            role="button"
+          >
+            Back to RX-Form
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
+export default Thanks;
