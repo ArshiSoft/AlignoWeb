f20b3e68f43f2577afc6e5dddd02627a5c8dd021
MugheesAhmad90 <mugheezk2@gmail.com>
2022-10-13 03:16:24 +0500

    Fixed errors in Navbar.Js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom';
-import Rxform from '../RxForm/Rxform';
+// import Rxform from '../RxForm/Rxform';
 import '../Navbar/Navbar.css';
+ /* eslint-disable */
 
 
 
@@ -10,7 +11,8 @@ function Navbar(){
         
          <nav className="navbar navbar-expand-lg navbar-dark navclass ">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Aligno</a>
+  
+    <a className="navbar-brand" >Aligno</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -22,21 +24,21 @@ function Navbar(){
             </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" >Link</a>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Another action</a></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" >Something else here</a></li>
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a className="nav-link disabled"  tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
       </ul>
       <form className="d-flex">
@@ -47,6 +49,7 @@ function Navbar(){
   </div>
   
 </nav>
+
  
        
 )
