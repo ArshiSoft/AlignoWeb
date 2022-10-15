d0934e138319e6eebc9dd2ba91cb0a43c6b6439e
ahsan ali <syedahsansaeed@gmail.com>
2022-10-15 13:10:32 +0500

    Apply phone number Validation in RsForm2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import './App.css';
+import "./App.css";
 // import Login from './components/Login';
-import Register from './components/Register/Register';
-import Dashboard from './components/Dashboard/Dashboard';
+import Register from "./components/Register/Register";
+import Dashboard from "./components/Dashboard/Dashboard";
 // import InventoryEditForm from './test';
-import Thanks from './components/Thanks';
-import Rxform from './components/RxForm/Rxform';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-
-
+import Thanks from "./components/Thanks";
+import Rxform from "./components/RxForm/Rxform";
+import Rxform2 from "./components/RxForm2/Rxform2";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
-
   return (
-
     <>
-
       <BrowserRouter>
-         {/* <nav>
+        {/* <nav>
           <Link to="/Login">Login</Link>
           <Link to="/Register">Register</Link>
           <Link to="/Dashboard">Dashboard</Link>
@@ -26,23 +21,21 @@ function App() {
         </nav> */}
 
         <Routes>
-          <Route path='/Rx-form' element={<Rxform />} />
-          <Route path='/thanks' element={<Thanks />} />
-          <Route path='/Dashboard' element={<Dashboard/>} />
-          <Route path='/Navbar' element={<Navbar/>} />
-          <Route path='/Register' element={<Register />} />
+          <Route path="/Rx-form" element={<Rxform />} />
+          <Route path="/Rx-form2" element={<Rxform2 />} />
+          <Route path="/thanks" element={<Thanks />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Navbar" element={<Navbar />} />
+          <Route path="/Register" element={<Register />} />
 
-      {/* <Route path='/test' element={<InventoryEditForm/>} /> */}
+          {/* <Route path='/test' element={<InventoryEditForm/>} /> */}
           {/* <Route path='/Login' element={<Login />} />
           
           <Route path='/Dashboard' element={<Dashboard />} /> */}
-         </Routes>
-         </BrowserRouter>
-
-
+        </Routes>
+      </BrowserRouter>
     </>
-
   );
-};
+}
 
 export default App;
