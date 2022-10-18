60ccdf12ee7bb10d40e9d0b7a1d44b2bb7b1ba4d
Arshian Siddiqui <arshians06@gmail.com>
2022-10-18 22:23:00 +0500

    fixed rx form view table data

--- src/components/Rx-Form-Data/Modules/RxTable.jsx
+++ src/components/Rx-Form-Data/Modules/RxTable.jsx
@@ -1,73 +1,66 @@
-import React from "react";
-import DataTable from "react-data-table-component";
-import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
+import React from 'react';
+import DataTable from 'react-data-table-component';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
+const RxTable = () => {
+	const [rxdata, setRxData] = useState([]);
 
+	const getRxData = async () => {
+		try {
+			const response = await axios.get('https://server.aligno.co/api/rx-form');
+			setRxData(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
+	const columns = [
+		{
+			name: 'Clinic Name',
+			selector: (row) => row.clinicname,
+		},
+		{
+			name: 'Doctor Name',
+			selector: (row) => row.doctorname,
+		},
+		{
+			name: 'Doctor Email',
+			selector: (row) => row.doctoremail,
+		},
+		{
+			name: 'Doctor Phone',
+			selector: (row) => row.doctornumber,
+		},
+		{
+			name: 'Patient Name',
+			selector: (row) => row.patientname,
+		},
+		{
+			name: 'Patient Email',
+			selector: (row) => row.patientemail,
+		},
+		{
+			name: 'Patient Phone',
+			selector: (row) => {
+				var link = 'http://wa.me/' + row.patientnumber;
+				return (
+					<a href={link} target='_blank'>
+						{row.patientnumber}
+					</a>
+				);
+			},
+		},
+		// {
+		// 	name: 'Patient Name',
+		// 	selector: (row) => <img width={50} height={50} src={row.flag} />,
+		// },
+	];
+	useEffect(() => {
+		getRxData();
+	}, []);
 
-const  RxTable =() =>{
-
-    const [rxdata,setRxData] = useState([]);
-
-    const getRxData = async () => {
-      try{
-        const response = await axios.get('https://restcountries.com/v2/all');
-        setRxData(response.data);
-      } catch(error){
-    console.log(error);
-      }
-    };
-    
-    
-
-   
-
-
-   
-    
-    
-    const columns = [
-      {
-        name: "Country Name",
-        selector : (row) => row.name,
-      },
-      {
-        name: "Country Native Name",
-        selector : (row) => row.nativeName,
-      },
-      {
-        name: "Doctor Number",
-        selector : (row) => row.capital,
-      },
-    //   {
-    //     name: "Doctor Email",
-    //     selector : (row) => row.doctoremail,
-        
-    //   },
-    //   {
-    //     name: "Patient Id",
-    //     selector : (row) => row.patienid,
-    //   },
-      {
-        name: "Patient Name",
-        selector : row => <img width={50} height={50} src={row.flag}/>
-        
-      },
-    //   {
-    //     name: "Patient Number",
-    //     selector : (row) => row.patientnumber,
-        
-    //   },
-    
-    ];
-    useEffect(() => {
-        getRxData();
-      },[]);
-
-    return <DataTable columns={columns} data={rxdata}  />;
-
-
+	return <DataTable columns={columns} data={rxdata.User} />;
 };
 export default RxTable;
