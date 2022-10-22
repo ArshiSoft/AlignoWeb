deb794ff5e2025042feec7efb906e884b5afef04
MugheesAhmad90 <mugheezk2@gmail.com>
2022-10-22 05:27:42 +0500

    Data shows on Details Button

--- src/components/Rx-Form-Data/Modules/RxTable.jsx
+++ src/components/Rx-Form-Data/Modules/RxTable.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Modal, Button,} from 'react-bootstrap';
+import './RxTable.css';
 
 
 
@@ -11,8 +13,40 @@ import { useNavigate } from 'react-router-dom';
 
 const RxTable = () => {
 	const [rxdata, setRxData] = useState([])
+	const [pending, setPending] = React.useState(true);
+	const [rows, setRows] = React.useState([]);
+	const [modalInfo, setModalInfo] = useState([]);
+	const [showModal, setShowModal] = useState(false);
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
 
+	React.useEffect(() => {
+		const timeout = setTimeout(() => {
+			setRows(rxdata);
+			setPending(false);
+		}, 2000);
+		return () => clearTimeout(timeout);
+	}, []);
+
+	// const rowEvents = {
+	// onClick : (e ,row) => {
+	// 	console.log(row)
+	// }
+	// }
+	const handleButtonClick = (e, row) => {
+		e.preventDefault();
+		console.log("Row Id", row);
+		setModalInfo(row)
+		toggelTrueFalse()
+	};
+
+	const toggelTrueFalse = () => {
+		setShowModal(handleShow);
+	};
+	
 
 
 	const getRxData = async () => {
@@ -25,6 +59,10 @@ const RxTable = () => {
 	};
 
 	const columns = [
+		// {
+		// 	name: 'ID',
+		// 	selector: (row) => row._id,
+		// },
 		{
 			name: 'Clinic Name',
 			selector: (row) => row.clinicname,
@@ -60,15 +98,39 @@ const RxTable = () => {
 				);
 			},
 		},
+		// {
+		// 	name: 'Full Data',
+		// 	cell: ( row ) => (
+		// 		<button className='btn-sm btn btn-primary' onClick={(e) => history('/Rx-data')(e ,row._id)}>
+		// 			Details
+		// 		</button>
+		// 	)
+		// },
 		{
-			name: 'Full Data',
-			cell: ({ row }) => (
-				<button className='btn-sm btn btn-primary' onClick={e => history('/Rx-data')(e.row.rxdata.User)}>
-					Details
+			name: "Actions",
+			button: true,
+			cell: (row) => (
+				<button
+					className="btn btn-outline btn-sm btn-primary"
+					onClick={(e) => handleButtonClick(e, row)}
+				>
+					View
 				</button>
-			)
+			),
 		}
+		// {
+		// 	name:"Action",
+		//   cell: (row: { _id: any }) => (
+		// 	  <>
+		// 	<span onClick={() => handleButtonClick(row._id)} className='btn btn-primary'><Pencil/></span>{'     '}
+		// 	<span onClick={() => handleButtonClick(row._id)} className='btn btn-danger'><Trash/></span>
+		// 	</>
+		//   ),
 
+		//   ignoreRowClick: true,
+		//   allowOverflow: true,
+		//   button: true,
+		// },{/*end*/}
 		// {
 		// 	name: 'Patient Name',
 		// 	selector: (row) => <img width={50} height={50} src={row.flag} />,
@@ -79,16 +141,60 @@ const RxTable = () => {
 	}, []);
 	const history = useNavigate();
 
-	return <DataTable
+	const ModalContent = () => {
+		return (
+			
+			<Modal  dialogClassName="my-modal" show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>
+						
+					</Modal.Title>Mughees</Modal.Header>
+				<Modal.Body>
+<h1>Rx Data</h1>
+<ul>
+	<ol>Clinic Name : {modalInfo.clinicname}</ol>
+	<ol>Doctor Name : {modalInfo.doctorname}</ol>
+	<ol>Patient Name : {modalInfo.patientname}</ol>
+	<ol>Doctor Number :{modalInfo.doctornumber}</ol>
+	<ol>Paient Number : {modalInfo.patientnumber}</ol>
+	<ol>Patient ID : {modalInfo.patienid}</ol>
+	<ol>Doctor Email : {modalInfo.doctoremail}</ol>
+	<ol>Patient Email : {modalInfo.patientemail}</ol>
+	<ol>Gender : {modalInfo.gender}</ol>
+	<ol>Bridges Implant : {modalInfo.bridgesimplant}</ol>
+	<ol>MRCIR : {modalInfo.mrciil}</ol>
+	</ul>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>Close</Button>
+				</Modal.Footer>
+
+
+			</Modal>
+		);
+	};
+
+	return (
+		<div className='modalcontent'>
+	<DataTable
 		title="Rx-Data"
 		columns={columns}
 		data={rxdata.User}
 		pagination
 		pointerOnHover={true}
 		highlightOnHover={true}
+		// rowEvent= {handleButtonClick}
+		progressPending={pending}
+
 	
+		
 	/>;
+		{ show ? <ModalContent/> : null }
+	</div>
+	);
+	
 
 };
+
 
 export default RxTable;
