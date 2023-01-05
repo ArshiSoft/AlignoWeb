ee9428cd6589ab4e1214c1c4b29b02836a261a90
MugheesAhmad90 <mugheezk2@gmail.com>
2023-01-06 02:55:40 +0500

    Menu Changing

--- src/components/Dashboard/Modules/Aside/AsideMenu.js
+++ src/components/Dashboard/Modules/Aside/AsideMenu.js
@@ -9,6 +9,7 @@ import {
   MenuUnfoldOutlined,
   FormOutlined,
   DatabaseOutlined,
+  FolderViewOutlined,
 } from "@ant-design/icons";
 import { DownOutlined, SmileOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
@@ -59,45 +60,96 @@ const items = [
     <Link to="/" className="mb-1">
       <DashboardOutlined />
     </Link>
+	
     // getItem('Form1', 'g1', null, [getItem('Rx View','1', [<Link to='/Rx-view'/>]), getItem('Rx Form','2', [<Link to='/Rx-form'/>],)], 'group'),
     // getItem('Item 2', 'g2', null, [getItem('NO', '3',[<Link to='/Rx-view'/>]), getItem('Option 4', '4',[<Link to='/Rx-form'/>])], 'group'),
   ),
-  getItem("Form", "sub2", <FormOutlined />, [
-    // getItem(
-    //   "Form1",
-    //   "g1",
-    //   null,
-    getItem("Rx Form", "2", [<Link to="/Rx-form" />]),
-    getItem("Clinic Form", "1", [<Link to="/Clinic-form" />]),
+  getItem(
+    "Candidate",
+    "Cand",
+    <Link to="/Candidate" className="mb-1">
+      <UserOutlined />
+    </Link>
+	
+    // getItem('Form1', 'g1', null, [getItem('Rx View','1', [<Link to='/Rx-view'/>]), getItem('Rx Form','2', [<Link to='/Rx-form'/>],)], 'group'),
+    // getItem('Item 2', 'g2', null, [getItem('NO', '3',[<Link to='/Rx-view'/>]), getItem('Option 4', '4',[<Link to='/Rx-form'/>])], 'group'),
+  ),
+  getItem(
+    "Rx Form",
+    "RxForm",
+    <Link to="/Rx-form" className="mb-1">
+      <FormOutlined />
+    </Link>
+	
+    // getItem('Form1', 'g1', null, [getItem('Rx View','1', [<Link to='/Rx-view'/>]), getItem('Rx Form','2', [<Link to='/Rx-form'/>],)], 'group'),
+    // getItem('Item 2', 'g2', null, [getItem('NO', '3',[<Link to='/Rx-view'/>]), getItem('Option 4', '4',[<Link to='/Rx-form'/>])], 'group'),
+  ),
+  getItem(
+    "Rx View",
+    "RxView",
+    <Link to="/Rx-view" className="mb-1">
+      <FolderViewOutlined />
+    </Link>
+	
+    // getItem('Form1', 'g1', null, [getItem('Rx View','1', [<Link to='/Rx-view'/>]), getItem('Rx Form','2', [<Link to='/Rx-form'/>],)], 'group'),
+    // getItem('Item 2', 'g2', null, [getItem('NO', '3',[<Link to='/Rx-view'/>]), getItem('Option 4', '4',[<Link to='/Rx-form'/>])], 'group'),
+  ),
+  getItem(
+    "Clinic-View",
+    "ClinicView",
+    <Link to="/Clinic" className="mb-1">
+      <FolderViewOutlined />
+    </Link>
+	
+    // getItem('Form1', 'g1', null, [getItem('Rx View','1', [<Link to='/Rx-view'/>]), getItem('Rx Form','2', [<Link to='/Rx-form'/>],)], 'group'),
+    // getItem('Item 2', 'g2', null, [getItem('NO', '3',[<Link to='/Rx-view'/>]), getItem('Option 4', '4',[<Link to='/Rx-form'/>])], 'group'),
+  ),
+  getItem(
+    "Clinic Form",
+    "Clinic-form",
+    <Link to="/Clinic-form" className="mb-1">
+      <FormOutlined />
+    </Link>
+	
+    // getItem('Form1', 'g1', null, [getItem('Rx View','1', [<Link to='/Rx-view'/>]), getItem('Rx Form','2', [<Link to='/Rx-form'/>],)], 'group'),
+    // getItem('Item 2', 'g2', null, [getItem('NO', '3',[<Link to='/Rx-view'/>]), getItem('Option 4', '4',[<Link to='/Rx-form'/>])], 'group'),
+  ),
+//   getItem("Form", "sub2", <FormOutlined />, [
+//     // getItem(
+//     //   "Form1",
+//     //   "g1",
+//     //   null,
+//     getItem("Rx Form", "2", [<Link to="/Rx-form" />]),
+//     getItem("Clinic Form", "1", [<Link to="/Clinic-form" />]),
 
-    getItem("Candidate", "5", [<Link to="/Am-I-a-Candidate" />]),
-    // "group"
-    // ),
-    // getItem(
-    //   "Data View",
-    //   "g2",
-    //   null,
-    //   [
+//     getItem("Candidate", "5", [<Link to="/Am-I-a-Candidate" />]),
+//     // "group"
+//     // ),
+//     // getItem(
+//     //   "Data View",
+//     //   "g2",
+//     //   null,
+//     //   [
 
-    //   ],
-    //   "group"
-    // ),
-  ]),
-  getItem("Data View", "sub3", <DatabaseOutlined />, [
-    getItem("Rx-View", "3", [<Link to="/Rx-view" />]),
-    getItem("CLinic-View", "4", [<Link to="/Clinic" />]),
+//     //   ],
+//     //   "group"
+//     // ),
+//   ]),
+//   getItem("Data View", "sub3", <DatabaseOutlined />, [
+//     getItem("Rx-View", "3", [<Link to="/Rx-view" />]),
+//     getItem("CLinic-View", "4", [<Link to="/Clinic" />]),
 
-    // getItem("Submenu", "sub4", null, [
-    //   getItem("Option 7", "7"),
-    //   getItem("Option 8", "8"),
-    // ]),
-  ]),
-  getItem("Navigation Three", "sub4", <SettingOutlined />, [
-    getItem("Option 9", "9"),
-    // getItem("Option 10", "10"),
-    // getItem("Option 11", "11"),
-    // getItem("Option 12", "12"),
-  ]),
+//     // getItem("Submenu", "sub4", null, [
+//     //   getItem("Option 7", "7"),
+//     //   getItem("Option 8", "8"),
+//     // ]),
+//   ]),
+//   getItem("Navigation Three", "sub4", <SettingOutlined />, [
+//     getItem("Option 9", "9"),
+//     // getItem("Option 10", "10"),
+//     // getItem("Option 11", "11"),
+//     // getItem("Option 12", "12"),
+//   ]),
 ];
 
 const AsideMenu = () => {
@@ -123,6 +175,7 @@ const AsideMenu = () => {
             {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
           </Button> */}
         <Sider
+		collapsible
           breakpoint="lg"
           collapsedWidth="0"
           onBreakpoint={(broken) => {	
